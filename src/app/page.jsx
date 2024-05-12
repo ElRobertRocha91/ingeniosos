c8251686f779ac52ea5b9b04942cd4576732680a
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2024-05-12 13:14:04 -0300

    Adding components About

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,16 @@
 import Carousel from "../components/Carousel/Carousel";
 import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
 
 // Load the courses of database from Server-Side Rendering 
 async function loadCourses() {
   const res = await fetch("http://localhost:3000/api/cursos");
   // console.log(res);
-  if(!res.ok){
+  if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
   const data = await res.json();
-  
+
   return data;
 }
 
@@ -17,12 +18,10 @@ export default async function Home() {
   const courses = await loadCourses();
   return (
     <main className="h-screen">
-      <div className="absolute w-full h-full">
-        <h1>Ingeniosos</h1>
-        <div>{courses}</div>
-      </div>
-      <Carousel />
-      <About />
+      <seccion className="absolute w-full h-full">
+        <About />
+        <Contact />
+      </seccion>
     </main>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,19 @@
 export default function About() {
-    return(
-        <div className="p-2 text-center">
-            Este es el componente About
-        </div>
+    return (
+        <seccion>
+            <div className="bg-pink-300">
+                <h1 className="text-rose-500 text-2xl font-bold text-center p-8">Sobre Nosotros</h1>
+                <div className="p-10">
+                    <p className="text-zinc-600 font-mono text-justify text-lg">
+                        Somos un centro de apoyo escolar enfocados en acompañar el recorrido escolar de los niños, adolescentes y sus familias,
+                        promoviendo su permanencia en la escuelas.
+                        Brindamos soporte escolar en todas las materias de la primaria y secundaria, trabajamos para que cada alumno vaya adquiriendo mayores niveles de autonomia como estudiante.
+                        <br />
+                        Ofrecemos apoyo psicopedagogico para aquellos niños/as o jovenes con necesidades educativas especiales. Brindando una atención individual conformada por nuestro equipo conformado
+                        por profesionales y docentes especializados.
+                    </p>
+                </div>
+            </div>
+        </seccion>
     )
 }
